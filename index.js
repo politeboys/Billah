dbf052ad30a8de17b8e2e096f1170613c0f762dd
Your Name <you@example.com>
2020-08-10 12:47:26 +0600

    17th

--- index.js
+++ index.js
@@ -18,13 +18,22 @@ function closeNav() {
 }
 
 // PARALUX EFFECT
+const paralux = document.getElementById('me');
+
 window.addEventListener('scroll', function(){
-    const paralux = document.querySelector('.details');
     let scrollPosition = window.pageYOffset;
 
-    paralux.style.transform = 'translateY(' + scrollPosition * -.5 + 'px)' ;
+    paralux.style.backgroundPositionY = scrollPosition * 0.7 + 'px' ;
 
     
-    const me = document.querySelector('.me');
-    me.style.transform = 'translateX(' + scrollPosition * -.1 + 'px)' ;
+    // const me = document.querySelector('.me');
+    // me.style.transform = 'translateX(' + scrollPosition * -.1 + 'px)' ;
 })
+
+// JQUERY PARALUX EFFECT
+// $(window).scroll(function(){
+//     var scroll_Position = $(window).scrollTop()/2;
+//     $('header').css({
+//         'background-position-y' : - scroll_Position + 'px',
+//     })
+// })
